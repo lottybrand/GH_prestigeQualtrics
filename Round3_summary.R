937188ebceae0a4d9de68b0a7ca6165af5702726
Charlotte Brand <git>
2017-10-25 18:11:42 +0100

    First shot at analysis seems to support one of the (main!) predictions at least... nicely (and bizarrely) contrasting Round 2's results. However cannot get multi level model to run, even with non-centred parameterisation. Perhaps due to many ppts only copying once, and a rare few copying 30-40 times.

---
+++ Round3_summary.R
@@ -0,0 +1,215 @@
+#### Qualtrics Prestige Study #####
+
+#Data file for handling the various Qualtrics output for our prestige study. 
+#first is just a summary file, followed by the full "info choice" data for the choices ppts made
+#Finally need to dig into when the "most copied" or "highest scorer" was chosen
+
+Round3Summary <- read.delim('Round3summary_stuff.txt')
+
+scoreMeans = tapply(Round3Summary$o_score, list(Round3Summary$Condition),mean)
+scoreMeans
+
+copyFreqMeans = tapply(Round3Summary$CopyFreq, list(Round3Summary$Condition),mean)
+copyFreqMeans
+  
+copyCopMeans = tapply(Round3Summary$CopCopied, list(Round3Summary$Condition),mean)
+copyCopMeans
+
+copyFreqSums = tapply(Round3Summary$CopyFreq, list(Round3Summary$Condition),sum)
+copyFreqSums
+
+copyCopSums = tapply(Round3Summary$CopCopied, list(Round3Summary$Condition),sum)
+copyCopSums
+
+copyHobSums = tapply(Round3Summary$CopHobs, list(Round3Summary$Condition),sum)
+copyHobSums
+
+copyScoreSums = tapply(Round3Summary$CopScore, list(Round3Summary$Condition),sum)
+copyScoreSums
+
+#create simple summary table
+Condition <- c("A", "B","C") 
+CopyTotal <- c(187,107,198)
+CopiedCopy <- c(86,62,36)
+CopiedHobby <- c(99,45,"na")
+CopiedScore <- c("na","na",161)
+SummaryInfo <- (data.frame(Condition, CopyTotal, CopiedCopy, CopiedHobby, CopiedScore))
+
+#create secondn summary table to visualise the summary data
+Condition <- c("A","A","A","B","B","B","C","C","C") 
+CopyInfo<- c("Chobby","Ccopy","Cscore","Chobby","Ccopy","Cscore","Chobby","Ccopy","Cscore")
+CopySum <- c(99,86,0,45,62,0,0,36,161)
+SummaryInfo2 <- (data.frame(Condition, CopyInfo, CopySum))
+
+Condition <-SummaryInfo2$Condition
+CopySum <- SummaryInfo2$CopySum
+CopyInfo <- SummaryInfo2$CopyInfo
+
+#plot the summary info
+sumPlot <- ggplot(SummaryInfo2, aes(Condition, CopySum, fill= CopyInfo)) +
+  stat_summary(fun.y=sum, geom = "bar", position = "dodge") 
+sumPlot
+
+
+
+
+#overall score models in rethinking
+library(rethinking)
+
+
+nullModel <- map2stan(
+  alist(o_score ~ dnorm(mu, sigma),
+        mu <- a,
+        a ~ dnorm(0,10),
+        sigma ~ dunif(0,10)),
+  data=Round3Summary, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(nullModel)
+
+#create condition A as baseline: 
+Round3Summary$CondB <- ifelse(Round3Summary$Condition =="B", 1, 0)
+Round3Summary$CondC <- ifelse(Round3Summary$Condition =="C", 1, 0)
+
+condModel <- map2stan(
+  alist(o_score ~ dnorm(mu, sigma),
+        mu <- a + b_b*CondB + b_c*CondC,
+        a ~ dnorm(0,10),
+        c(b_b,b_c) ~ dnorm(0,1),
+        sigma ~ dunif(0,10)),
+  data=Round3Summary, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(condModel)
+compare(condModel,nullModel)
+
+
+#overall copyFrequency models:
+#rethinking
+
+
+nullModel_cf <- map2stan(
+  alist(CopyFreq ~ dnorm(mu, sigma),
+        mu <- a,
+        a ~ dnorm(0,10),
+        sigma ~ dunif(0,10)),
+  data=Round3Summary, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(nullModel_cf)
+
+
+condModel_cf <- map2stan(
+  alist(CopyFreq ~ dnorm(mu, sigma),
+        mu <- a + b_b*CondB + b_c*CondC,
+        a ~ dnorm(0,10),
+        c(b_b,b_c) ~ dnorm(0,1),
+        sigma ~ dunif(0,10)),
+  data=Round3Summary, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(condModel_cf)
+compare(condModel_cf,nullModel_cf)
+
+#copyPrestige models: 
+
+
+nullModel_cp <- map2stan(
+  alist(CopCopied ~ dnorm(mu, sigma),
+        mu <- a,
+        a ~ dnorm(0,10),
+        sigma ~ dunif(0,10)),
+  data=Round3Summary, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(nullModel_cp)
+
+
+condModel_cp <- map2stan(
+  alist(CopCopied ~ dnorm(mu, sigma),
+        mu <- a + b_b*CondB + b_c*CondC,
+        a ~ dnorm(0,10),
+        c(b_b,b_c) ~ dnorm(0,1),
+        sigma ~ dunif(0,10)),
+  data=Round3Summary, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(condModel_cp)
+compare(condModel_cp,nullModel_cp)
+
+#loading the info choice data isolated from the bigger qualtrics file: 
+
+infoChoiceData <- read.delim('info_choice.txt')
+
+#remove NA columns (questions on which no one copied):
+infoChoiceData <- Filter(function(x)!all(is.na(x)), infoChoiceData)
+
+colnames(infoChoiceData)
+
+#reshape the info choice data 
+prestigeData <- reshape(infoChoiceData, idvar = "idNum", 
+                 varying = list(c(19:236)),
+                 v.names = c("infoChoice"), 
+                 direction = "long")
+
+# the "time" column now referrs to different questions, 
+# although, these have all been named incrementally during reshape and don't relate back to the original Q numbers, hence "Qtype"
+# will have to fix this if wanting to control for diff questions (although v few copying events per Q)
+
+colnames(prestigeData)[27] <- "Qtype"
+
+#label the choice column more usefully from the qualtrics output
+prestigeData$choice <- ifelse((prestigeData$infoChoice=="Their score on the quiz"),"score",
+                          ifelse((prestigeData$infoChoice=="One of their top three hobbies"),"hobbies",
+                                 ifelse((prestigeData$infoChoice=="How many times they were copied"),"prestige","NA")))
+
+#now just isolating the data in which copying did occur:
+
+copyData = prestigeData[!prestigeData$choice == "NA",]
+
+copyData$choice_num <- ifelse((copyData$choice =="hobbies"),1,
+                              ifelse((copyData$choice =="prestige"),2,3))
+
+copyData$prestige <- ifelse((copyData$choice_num ==2), 1, 0)
+
+#create condition A as baseline: 
+copyData$CondB <- ifelse(copyData$Condition =="B", 1, 0)
+copyData$CondC <- ifelse(copyData$Condition =="C", 1, 0)
+
+#prestige models
+
+null_prestige <- map2stan(
+  alist(prestige ~ dbinom(1,p),
+        logit(p) <- a,
+        a ~ dnorm(0,10)
+  ),
+  data=copyData, warmup = 1000, iter=2000, chains=1, cores = 1)
+
+precis(null_prestige)
+
+#prestige model
+prestige_model <- map2stan(
+  alist(prestige ~ dbinom(1, p),
+        logit(p) <- a + b_b*CondB + b_c*CondC,  
+        a ~ dnorm(0,10),
+        c(b_b, b_c) ~ dnorm(0,4)
+  ),
+  data=copyData, warmup=1000, iter=6000, chains=1, cores=1 )
+
+precis(prestige_model)
+compare(null_prestige,prestige_model)
+
+#multi-level taking ppt into account? (should check for Q too?)
+prestige_ml <- map2stan(
+  alist(
+    prestige ~ dbinom(1, p),
+    logit(p) <- a + a_p[idNum]*sigma_p +
+      b_b*CondB +
+      b_c*CondC,
+    a ~ dnorm(0,10),
+    c(b_b,b_c) ~ dnorm(0,4),
+    a_p[idNum] ~ dnorm(0,1),
+    sigma_p ~ dcauchy(0,1)
+  ),
+  data=copyData, constraints=list(sigma_p="lower=0"),
+  warmup=1000, iter=2000, chains=3, cores=3)
+
+#won't run, possibly because most observations come from one idNum, but a few idNums provide many
+#observationsm e.g. 107 copied 41 times, shown in table below.  
+
+summary(copyData$idNum)
+table(copyData$idNum)
--- hob_correlations.R
+++ hob_correlations.R
@@ -1,10 +1,12 @@
 library(rethinking)
+library(lme4)
 
 setwd("~/Desktop/Postdoc/Experiments:Ideas/QualtricsStudy/GH_prestigeQualtrics")
 
 hobData <- read.delim("hobbies_and_scores.txt")
 
 Score <- hobData$Overall.Score
+
 
 hobData$cook <- ifelse(hobData$HOB_NUM1==4, 1, 0)
 hobData$read <- ifelse(hobData$HOB_NUM1==13, 1, 0)
@@ -19,9 +21,106 @@ gaming <- hobData$gaming
 hobbiesModel <- map2stan(
   alist(Score ~ dnorm(mu, sigma),
         mu <- a + b_c*cook + b_r*read + b_f*football * b_g*gaming,
-        a ~ dnorm(61,50),
+        a ~ dnorm(61,10),
         c(b_c, b_r, b_f, b_g) ~ dnorm(0,10),
-        sigma ~ dunif(0,50)),
+        sigma ~ dunif(0,10)),
   data=hobData, warmup = 1000, iter=2000, chains=1, cores = 1)
 
 precis(hobbiesModel)
+
+hobs <- glm(Score ~ cook + read + football + gaming, family = gaussian)
+hobs
+confint(hobs)
+
+hobData$cook2 <- ifelse(hobData$HOB_NUM2==1, 1, 0)
+hobData$game2 <- ifelse(hobData$HOB_NUM2==2, 1, 0)
+hobData$gym2 <- ifelse(hobData$HOB_NUM2==3, 1, 0)
+hobData$reading2 <- ifelse(hobData$HOB_NUM2==4, 1, 0)
+hobData$running2 <- ifelse(hobData$HOB_NUM2==5, 1, 0)
+hobData$walking2 <- ifelse(hobData$HOB_NUM2==6, 1, 0)
+hobData$yoga2 <- ifelse(hobData$HOB_NUM2==7, 1, 0)
+
+cook2 <- hobData$cook2
+game2 <- hobData$game2 
+gym2 <- hobData$gym2 
+reading2 <- hobData$reading2 
+running2 <- hobData$running2 
+walking2 <- hobData$walking2 
+yoga2 <- hobData$yoga2
+
+hobs2 <- glm(Score ~ cook2 + game2 + gym2 + reading2 +running2 + walking2 + yoga2, family = gaussian)
+hobs2
+confint(hobs2)
+
+hobData$game3 <- ifelse(hobData$HOB_NUM3==1, 1, 0)
+hobData$swim3 <- ifelse(hobData$HOB_NUM3==2, 1, 0)
+hobData$read3 <- ifelse(hobData$HOB_NUM3==3, 1, 0)
+hobData$tv3 <- ifelse(hobData$HOB_NUM3==4, 1, 0)
+hobData$gym3 <- ifelse(hobData$HOB_NUM3==5, 1, 0)
+hobData$knit3 <- ifelse(hobData$HOB_NUM3==6,1,0)
+
+game3 <- hobData$game3
+swim3 <- hobData$swim3
+read3 <- hobData$read3
+tv3 <- hobData$tv3
+gym3 <- hobData$gym3
+knit3 <- hobData$knit3
+
+hobs3 <- glm(Score ~ game3 + swim3 + read3 + tv3 + gym3 + knit3, family = gaussian)
+hobs3
+confint(hobs3)
+
+hobDataALL <- read.delim("hobbies_and_scores_all.txt")
+Score <- hobDataALL$Overall.Score
+CAT <- hobDataALL$hob_cat
+ID <- hobDataALL$ID
+
+hobsALL <- glm(Score ~ CAT, family = gaussian)
+hobsALL
+confint(hobsALL)
+
+hobDataALL$cook <- ifelse(CAT=="B", 1, 0)
+hobDataALL$draw <- ifelse(CAT=="C", 1, 0)
+hobDataALL$cycle <- ifelse(CAT=="D", 1, 0)
+hobDataALL$dance <- ifelse(CAT=="E", 1, 0)
+hobDataALL$yoga <- ifelse(CAT=="F", 1, 0)
+hobDataALL$tv <- ifelse(CAT=="G", 1, 0)
+hobDataALL$game <- ifelse(CAT=="H", 1, 0)
+hobDataALL$football <- ifelse(CAT=="I", 1, 0)
+hobDataALL$fish <- ifelse(CAT=="J", 1, 0)
+hobDataALL$gym <- ifelse(CAT=="K", 1, 0)
+hobDataALL$walk <- ifelse(CAT=="L", 1, 0)
+hobDataALL$swim <- ifelse(CAT=="M", 1, 0)
+hobDataALL$run <- ifelse(CAT=="N", 1, 0)
+hobDataALL$garden <- ifelse(CAT=="O", 1, 0)
+hobDataALL$read <- ifelse(CAT=="P", 1, 0)
+
+stan_hobData <- hobDataALL[-c(2,4,6,7,8)]
+stan_hobData <- stan_hobData[-c(2,4)]
+Score <- stan_hobData$Overall.Score
+ID <- stan_hobData$ID
+cook <- stan_hobData$cook
+draw <- stan_hobData$draw
+cycle <- stan_hobData$cycle
+dance <- stan_hobData$dance
+yoga <- stan_hobData$yoga
+tv <- stan_hobData$tv
+game <- stan_hobData$game
+football <- stan_hobData$football
+fish <- stan_hobData$fish
+gym <- stan_hobData$gym
+walk <- stan_hobData$walk
+swim <- stan_hobData$swim
+run <- stan_hobData$run
+garden <- stan_hobData$garden
+read <- stan_hobData$read
+
+
+hobs_all <- map2stan(
+  alist(Score ~ dnorm(mu, sigma),
+        mu <- a + b_b*cook + b_c*draw + b_d*cycle + b_e*dance + b_f*yoga + b_g*tv + b_h*game + b_i*football + b_j*fish + b_k*gym + b_l*walk + b_m*swim + b_n*run + b_o*garden + b_p*read,
+        a ~ dnorm(61,20),
+        c(b_b, b_c, b_d, b_e, b_f, b_g, b_h, b_i, b_j, b_k, b_l, b_m, b_n, b_o, b_p) ~ dnorm(0,10),
+        sigma ~ dunif(0,50)
+  ),
+  data=stan_hobData, warmup = 1000, iter = 2000, chains = 1, cores = 1)
